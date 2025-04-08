58acd209897973eb082ad5baec9c39c64545cd50
AhgaresNoot <ahgares@gmail.com>
2025-04-08 16:01:04 +0200

    Add files via upload

---
+++ webserverAPI.ino
@@ -0,0 +1,221 @@
+#include <ESP8266WiFi.h>
+#include <Hash.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is connected to GPIO 4
+#define ONE_WIRE_BUS 2
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+// Variables to store temperature values
+String temperatureF = "";
+String temperatureC = "";
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "time.nist.gov", 3600);
+
+// Timer variables
+unsigned long lastTime = 0;  
+unsigned long timerDelay = 1000;
+
+// Replace with your network credentials
+const char* ssid = "iPhone von Julia";
+const char* password = "NUK14823";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+String readDSTemperatureC() {
+  // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
+  sensors.requestTemperatures(); 
+  float tempC = sensors.getTempCByIndex(0);
+
+  if(tempC == -127.00) {
+    Serial.println("Failed to read from DS18B20 sensor");
+    return "--";
+  } else {
+    Serial.print("Temperature Celsius: ");
+    Serial.println(tempC); 
+  }
+  return String(tempC);
+}
+
+String readDSTemperatureF() {
+  // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
+  sensors.requestTemperatures(); 
+  float tempF = sensors.getTempFByIndex(0);
+
+  if(int(tempF) == -196){
+    Serial.println("Failed to read from DS18B20 sensor");
+    return "--";
+  } else {
+    Serial.print("Temperature Fahrenheit: ");
+    Serial.println(tempF);
+  }
+  return String(tempF);
+}
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .ds-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+  </style>
+</head>
+<body>
+  <h2>ESP DS18B20 Server</h2>
+  <p>
+    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
+    <span class="ds-labels">Temperature Celsius</span> 
+    <span id="temperaturec">%TEMPERATUREC%</span>
+    <sup class="units">&deg;C</sup>
+  </p>
+  <p>
+    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
+    <span class="ds-labels">Temperature Fahrenheit</span>
+    <span id="temperaturef">%TEMPERATUREF%</span>
+    <sup class="units">&deg;F</sup>
+  </p>
+  <p> 
+    <span class="ds-labels">Uhrzeit:</span>
+    <span id="time">%TIME%</span>
+  </p>
+</body>
+<script>
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temperaturec").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperaturec", true);
+  xhttp.send();
+}, 1000) ;
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temperaturef").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperaturef", true);
+  xhttp.send();
+}, 1000) ;
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("time").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/time", true);
+  xhttp.send();
+}, 1000) ;
+</script>
+</html>)rawliteral";
+
+String processor(const String& var){
+  if(var == "TEMPERATUREC"){
+    return temperatureC;
+  }
+  else if(var == "TEMPERATUREF"){
+    return temperatureF;
+  }
+  return String();
+}
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println();
+  
+  // Start up the DS18B20 library
+  sensors.begin();
+
+  temperatureC = readDSTemperatureC();
+  temperatureF = readDSTemperatureF();
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temperaturec", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", temperatureC.c_str());
+  });
+  server.on("/temperaturef", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", temperatureF.c_str());
+  });
+  server.on("/time", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", timeClient.getFormattedTime().c_str());
+  });
+  //server.on("/all", HTTP_GET, [](AsyncWebServerRequest *request){
+  //  request->send_P(200, "JSON", jsonAllVars);
+  //});
+  server.on("/hello", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", "Hello World!");
+  });
+  // Start server
+  server.begin();
+
+  timeClient.begin();
+  timeClient.setTimeOffset(7200); // GMT+1
+
+  int retries = 0;
+  while (!timeClient.update() && retries < 10) {
+    Serial.println("Trying to sync time...");
+    timeClient.forceUpdate();
+    retries++;
+    delay(500);
+  }
+
+  if (!timeClient.isTimeSet()) {
+    Serial.println("Failed to get time from NTP.");
+  } else {
+    Serial.println("Time synchronized.");
+  }
+}
+ 
+void loop(){
+  if ((millis() - lastTime) > timerDelay) {
+    temperatureC = readDSTemperatureC();
+    temperatureF = readDSTemperatureF();
+    timeClient.update();
+    lastTime = millis();
+  }  
+}
